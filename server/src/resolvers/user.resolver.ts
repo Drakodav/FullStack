d0470b8c0ebeb29b936c47096bbf10009e129ea2
Drakodav <vmedves@gmail.com>
2020-09-18 23:33:49 +0100

    ✅finished registration

--- server/src/resolvers/user.resolver.ts
+++ server/src/resolvers/user.resolver.ts
@@ -11,6 +11,7 @@ import {
 } from "type-graphql";
 import { User } from "../entities/User";
 import argon2 from "argon2";
+import { EntityManager } from "@mikro-orm/postgresql";
 
 @InputType()
 class UsernamePassordInput {
@@ -70,7 +71,7 @@ export class UserResolver {
       return {
         errors: [
           {
-            field: "username",
+            field: "password",
             message: "lenght must be greater than three",
           },
         ],
@@ -78,13 +79,21 @@ export class UserResolver {
     }
 
     const hashedPassword = await argon2.hash(options.password);
-    const user = em.create(User, {
-      username: options.username,
-      password: hashedPassword,
-    });
+    let user;
     try {
-      await em.persistAndFlush(user);
+      const result = await (em as EntityManager)
+        .createQueryBuilder(User)
+        .getKnexQuery()
+        .insert({
+          username: options.username,
+          password: hashedPassword,
+          created_at: new Date(),
+          updated_at: new Date(),
+        })
+        .returning("*");
+      user = result[0];
     } catch (err) {
+      err.detail.includes("already exists");
       // duplicate username error
       if (err.code === "23505") {
         return {
